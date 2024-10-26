d3812299e8cec13e638b4912986a9212e624f456
Lucas <35698482+loukach@users.noreply.github.com>
2024-10-26 12:31:35 +0100

    Update components.js

--- js/components.js
+++ js/components.js
@@ -28,23 +28,24 @@ const Modal = ({ card, onClose }) => {
 };
 
 // Card Component
-const Card = () => {
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {cards.map((card, index) => (
-        <Card key={index}>
-          <CardHeader>
-            <CardTitle>{card.title}</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <p className="text-gray-500">{card.category}</p>
-            <p>{card.text}</p>
-            <div className="mt-4">
-              <div dangerouslySetInnerHTML={{ __html: card.fullContent }} />
+const Card = ({ category, title, text, fullContent, onClick }) => {
+    return (
+        <div 
+            className="group h-48 w-48 [perspective:1000px] cursor-pointer card-hover-effect"
+            onClick={() => onClick({ category, title, text, fullContent })}
+        >
+            <div className="relative h-full w-full transition-transform duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
+                {/* Front of card */}
+                <div className="absolute h-full w-full bg-black text-white rounded-lg p-4 [backface-visibility:hidden]">
+                    <div className="text-sm font-semibold text-blue-300 mb-2">{category}</div>
+                    <h2 className="text-xl font-bold text-center mt-8">{title}</h2>
+                </div>
+                
+                {/* Back of card */}
+                <div className="absolute h-full w-full bg-white text-black rounded-lg p-4 [transform:rotateY(180deg)] [backface-visibility:hidden] border-2 border-black">
+                    <p className="text-center mt-8 text-sm">{text}</p>
+                </div>
             </div>
-          </CardContent>
-        </Card>
-      ))}
-    </div>
-  );
+        </div>
+    );
 };
